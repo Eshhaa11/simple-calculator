44061e2b3d76e0d6efb3e3f6bff10aa3dc0e1e89
Esha Patel <Patellesha11@gmail.com>
2025-04-07 11:02:23 +0300

    added operator

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -26,9 +26,9 @@ function Calculator() {
       <input type="text" value={input} readOnly />
       <div>
         <button onClick={() => handleClick("1")}>1</button>
-        <button onClick={() => handleClick("8")}>8</button>
-        <button onClick={() => handleClick("9")}>9</button>
-        <button onClick={() => handleClick("/")}>/</button>
+        <button onClick={() => handleClick("2")}>2</button>
+        <button onClick={() => handleClick("3")}>3</button>
+        <button onClick={() => handleClick("-")}>-</button>
       </div>
       <div>
         <button onClick={() => handleClick("4")}>4</button>
